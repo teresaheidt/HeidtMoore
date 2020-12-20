a0359dbec8582c0e6dcfc784e22f55694371250e
Teresa Heidt <teresaheidt@hotmail.com>
2020-12-20 14:31:28 -0600

    added animation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Body= ()=>{
+  return(
+    <div id='body'>
+      <Header/>
+      <Card
+        className='rotate'
+        img='./imgs/IMG_8588.jpg'
+        // title='About the HeidtMoore'
+        // description='Welcome to our store. Shop till you drop!'
+      />
+      {/* <ContactContainer/> */}
+    </div>
+  )
+}
+
+const Header = () =>{
+  return(
+    <div className='header'>
+      <span className='header-title'>
+        
+      </span>
+      <br/>
+      <span className='header-text'>
+       
+      </span>
+    </div>
+  );
+}
+
+const Card = (props) =>{
+  return(
+    <div className={props.className} >
+      <div className='small-div'>
+        <i className={props.className}></i>
+        <img src={props.img} alt=''/>
+      </div>
+
+      <div className='big-div'>
+        <span className='div-title'>
+          {props.title}
+        </span>
+        <br/>
+        <span>
+          {props.description}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<Body/>, document.getElementById('root'));
